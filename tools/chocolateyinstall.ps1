ecb181ce253d8d042fa949be806d525da02a2881
Brendan Moran <b.daniel.moran@gmail.com>
2024-03-07 13:28:16 -0500

    Update bookmarks download process

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿#define tools dir
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bookmarksObjectUrl = https://sennecadeployments.blob.core.windows.net/bookmarks/jobrelease_bookmarks
+$bookmarksHtmlUrl = https://sennecadeployments.blob.core.windows.net/bookmarks/jobrelease_bookmarks.html
 
 try {
     
@@ -13,9 +15,9 @@ try {
 
     Write-Output "ChocoTemp folder exists, downloading bookmarks file"
 
-    #move the sales+eng_bookmarks(.json) file in the tools dir to the temp folder, with the name "Bookmarks"
-    Write-Output "Moving bookmarks file to C:\ProgramData\ChocoTemp"
-    Move-Item -Path "$toolsDir\jobrelease_bookmarks" -Destination "C:\ProgramData\ChocoTemp\Bookmarks"
+    #get the bookmarks file from the Azure Blob Storage and put it in c:\ProgramData\ChocoTemp as "Bookmarks"
+    Write-Output "Pulling bookmarks file to C:\ProgramData\ChocoTemp"
+    Invoke-WebRequest -Uri $bookmarksObjectUrl -OutFile "C:\ProgramData\ChocoTemp\Bookmarks"
 
 
     # check if the bookmarks file exists in the temp folder, and if it does, write success message, if it does not, throw error to catch block
@@ -27,9 +29,9 @@ try {
         Throw
     }
 
-    #finally, as a fail-safe, move the bookmarks.html from the tools directory to the public desktop for manual loading by the user if needed
-    Write-Output "Moving bookmarks html file to public desktop"
-    Move-Item -Path "$toolsDir\jobrelease_bookmarks.html" -Destination "C:\Users\Public\Desktop\Bookmarks.html"
+    #finally, as a fail-safe, pull the bookmarks.html from the Azure Blob Storage and put it on the public desktop
+    Write-Output "Pulling bookmarks html file to public desktop"
+    Invoke-WebRequest -Uri $bookmarksHtmlUrl -OutFile "$env:Public\Desktop\Bookmarks.html"
 } 
 catch {
     Write-Output "Error downloading bookmarks file from Azure Blob Storage"
