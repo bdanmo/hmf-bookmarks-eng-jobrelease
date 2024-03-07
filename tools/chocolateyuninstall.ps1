62e2e0996d4f28f8dbfe3f661c2e8d717b0aad32
Brendan Moran <b.daniel.moran@gmail.com>
2024-03-07 12:51:51 -0500

    Add new files and scripts for managing bookmarks

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,19 @@
+﻿#this script cannot remove bookmark files from the user's profile, so it will only remove the files from the public desktop and the temp folder
+
+#define path to bookmarks file in temp folder
+$bookmarksTemp = "C:\ProgramData\ChocoTemp\Bookmarks"
+
+#remove bookmark file from temp folder, if it exists
+if (Test-Path -Path $bookmarksTemp) {
+    Write-Output "Removing $bookmarksTemp"
+    Remove-Item -Path $bookmarksTemp
+}
+
+#remove "sales+eng_bookmarks.html" from public desktop, if it exists
+if (Test-Path -Path "C:\Users\Public\Desktop\Bookmarks.html") {
+    Write-Output "Removing C:\Users\Public\Desktop\Bookmarks.html"
+    Remove-Item -Path "C:\Users\Public\Desktop\Bookmarks.html"
+}
+
+
+
